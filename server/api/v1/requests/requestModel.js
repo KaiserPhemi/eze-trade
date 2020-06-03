9e5fe4c5d28ba27884f7fa7c66db17c9d2b8dd98
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-06-03 23:54:31 +0100

    ft(backend)
    - add model

---
+++ server/api/v1/requests/requestModel.js
@@ -0,0 +1,35 @@
+// third-party libraries
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+/**
+ * @desc  default schemma for all requests
+ */
+const tradeRequestModel = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  condition: {
+    type: String,
+    required: true,
+  },
+  storageCapacity: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+// model(s)
+const BuyRequest = model("BuyRequest", tradeRequestModel);
+const SellRequest = model("SellRequest", tradeRequestModel);
+
+export default { BuyRequest, SellRequest };
